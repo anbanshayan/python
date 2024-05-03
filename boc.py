26f3255894643762df1bf1bb270aa16eabf0307c
anbanshayan <anbanshayan3@gmail.com>
2024-05-03 23:54:43 +0530

    cal_update #$^%

---
+++ boc.py
@@ -0,0 +1,4 @@
+choices = ["apple", "banana", "cherry"]
+separator = ", "  # You can change this to modify the spacing
+
+print(separator.join(choices))
--- samplecal.py
+++ samplecal.py
@@ -1,5 +1,19 @@
 #selecting operator in calculator
-print("Select your operand : + - * /")
+choices=['+','-','*','/','^','%','#','$']
+seperator=','
+print(seperator.join(choices))
+
+#As you can see, the join method effectively combines the elements from the choices list into a single string separated by commas, achieving the desired output without square brackets.
+
+sign=input("Choose your operand :")
+
+if sign in choices:
+#above lines chcks if entered is available in choices
+    num1=int(input("Enter first number : "))
+    num2=int(input("Enter second number : "))
+
+else:
+    print("Choose valid operand from",choices)
 
 def add(a,b):
     print(a+b)
@@ -12,8 +26,32 @@ def multiply(a,b):
 
 def divide(a,b):
     if b==0:
-        print("Error!! can't divide by 0")
+        print("Error!! Can't divide by 0")
     else:
         print(a/b)
 
-divide(1,0)
+def power(a,b):
+    print(a**b)
+
+def remainder(a,b):
+    print(a%b)
+
+def terminate():
+    return choices
+if sign=='+':
+    result = add(num1,num2)
+
+elif sign=='-':
+    result = subtract(num1,num2)
+
+elif sign=='*':
+    result = multiply(num1,num2)
+
+elif sign=='/':
+    result = divide(num1,num2)
+
+elif sign=='^':
+    result = power(num1,num2)
+
+elif sign=='/':
+    result = remainder(num1,num2)
