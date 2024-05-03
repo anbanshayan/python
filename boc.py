9d666d7dcb2c131e58d661b19ef78035c4d07451
anbanshayan <anbanshayan3@gmail.com>
2024-05-03 23:58:00 +0530

    cal_update #$^%1

--- boc.py
+++ boc.py
@@ -1,4 +1,4 @@
 choices = ["apple", "banana", "cherry"]
-separator = ", "  # You can change this to modify the spacing
+#separator = ", "  # You can change this to modify the spacing
 
-print(separator.join(choices))
+print(",".join(choices))
