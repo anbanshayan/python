4c78f772f9b06e1f29dd84111886954da7151df9
anbanshayan <anbanshayan3@gmail.com>
2025-01-26 23:22:54 +0530

    crash course

---
+++ crash_course/main.py
@@ -0,0 +1,5 @@
+number = int(input("I'm thinking a number! Try to guess the number I'm thinking of : "))
+if(number == 25):
+    print("You are winner!!")
+else:
+    print("Better Luck Next Time!!")
