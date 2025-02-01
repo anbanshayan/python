fd6fd7fe12d1eca61f0c2cdc68959d5af9ec4099
anbanshayan <anbanshayan3@gmail.com>
2025-02-01 23:59:50 +0530

    odd or even finder made into dunction

--- crash_course/num.py
+++ crash_course/num.py
@@ -1,6 +1,5 @@
-print("Hello World!!")
-num = int(input("Enter A number :"))
-if(num % 2 ==0):
-    print(num," is an even number")
-else:
-    print(num, "is an odd number")
+def oddEven(num):
+    if(num % 2 ==0):
+        print(num," is an even number")
+    else:
+        print(num, "is an odd number")
