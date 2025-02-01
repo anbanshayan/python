1195eff092d0271aabdcc9ced580049745cc81c5
anbanshayan <anbanshayan3@gmail.com>
2025-02-02 00:30:16 +0530

    guessMyNumber createdgit add .

--- crash_course/num.py
+++ crash_course/num.py
@@ -5,3 +5,16 @@ def oddEven(num):
         print(num, "is an odd number")
 
 oddEven(52)
+
+n = int(input("Enter a number"))
+
+while(n != 25):
+    if(n>25):
+        print("Your guess is high")
+    elif(n<25):
+        print("Your guess is low")
+    
+    n = int(input("Try again : "))
+
+print("Congrats!! You have guessed the right one")
+
