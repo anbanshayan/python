384211a2f1d852c0e66d7793b9481a8e66d53507
anbanshayan <anbanshayan3@gmail.com>
2025-02-04 00:08:34 +0530

    My Simple GPA Calculator Created

--- crash_course/gpa_cal.py
+++ crash_course/gpa_cal.py
@@ -1,8 +1,14 @@
-course = float(input("Enter GPA"))
-credits= int(input("Enter Resprctive GPA "))
+count = int(input("No of courses to be calculated = "))
 
-current_gpa = course*credits
+gpa_sum = 0
+credits_sum=0
 
-print("Your GPA is :",current_gpa)
-             
-for 
+for i in range(1,count+1):
+    course = float(input("Enter Allocate GPA =>"))
+    credits= int(input("Enter Credits for GPA =>"))
+    current_gpa = course*credits
+
+    gpa_sum += current_gpa
+    credits_sum += credits
+
+print("Your Average GPA is :",gpa_sum/credits_sum)
