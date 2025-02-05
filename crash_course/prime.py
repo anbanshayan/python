d05e7da87472112ed4876f75b7b9ee52473947b2
anbanshayan <anbanshayan3@gmail.com>
2025-02-06 00:12:34 +0530

    prime number finding program

--- crash_course/prime.py
+++ crash_course/prime.py
@@ -2,6 +2,15 @@
 
 num = int(input("Enter a number :"))
 
+count = 0
+
 for i in range(1,num+1):
-    if num % 1 ==0 and num % num ==0:
-        count += count
+    value = num % i
+
+    if value == 0:
+        count += 1
+
+if count ==2:
+    print("It's a prime number! :)")
+else:
+    print("No it's not a prime number :(")
