a603c710334797529eea170917fecd06847bb384
anbanshayan <anbanshayan3@gmail.com>
2025-02-07 23:59:48 +0530

    finding greater in three numbers

---
+++ crash_course/largest.py
@@ -0,0 +1,12 @@
+#Write a program that takes three numbers as input and prints the largest of them.
+
+num1 = int(input("Enter num1 "))
+num2 = int(input("Enter num2 "))
+num3 = int(input("Enter num3 "))
+
+if num1 > num2:
+    print(num1,"is greater than ",num2)
+
+if num2 > num3:
+        print(num2,"is greater than ",num3)
+
