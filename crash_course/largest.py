e53615412231e8642d6ebb93150a87fd7d22a99e
anbanshayan <anbanshayan3@gmail.com>
2025-02-08 23:58:52 +0530

    finding greater in three numbers

--- crash_course/largest.py
+++ crash_course/largest.py
@@ -7,6 +7,8 @@ num3 = int(input("Enter num3 "))
 if num1 > num2:
     print(num1,"is greater than ",num2)
 
-if num2 > num3:
-        print(num2,"is greater than ",num3)
+elif num2 > num3:
+    print(num2,"is greater than ",num3)
+elif num3>num2:
+    print(num3,"is greater than ",num2)
 
