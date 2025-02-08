bff3b81c380fb32dd61fc8962b060273a9289200
anbanshayan <anbanshayan3@gmail.com>
2025-02-09 00:10:33 +0530

    finding greater in given  three numbers done!

--- crash_course/largest.py
+++ crash_course/largest.py
@@ -4,12 +4,13 @@ num1 = int(input("Enter num1 "))
 num2 = int(input("Enter num2 "))
 num3 = int(input("Enter num3 "))
 
-if num1 > num2:
-    print(num1,"is greater than ",num2)
+if num1 > num2 and num1 > num3:
+    print(num1,"is greatest of all")
 
-elif num2 > num3:
-    print(num2,"is greater than ",num3)
-elif num3>num2:
-    print(num3,"is greater than ",num2)
-elif num3>num1:
-    print(num3,"is greater than ",num2)
+elif num2 >num3 and num2 > num1:
+    print(num2, "is greatest of all")
+    
+else:
+    print(num3, "is greatest of all")
+
+
